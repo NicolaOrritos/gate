4cf0983ba0a48a2360cfe1f8855df9b16b88b5c2
Nicola Orritos <nicola.orritos@gmail.com>
2015-08-17 12:18:01 +0200

    Changed to a simpler stages loading with builtin helper stages

--- main.js
+++ main.js
@@ -4,6 +4,21 @@
 
 var Logger   = require('bunyan');
 var progenic = require('progenic');
+var docopt   = require('docopt').docopt;
+
+
+var DEFAULT_CONF_DIR = '.';
+
+var options = 'Start the Gate server \n'
+            + '  \n'
+            + 'Usage: \n'
+            + '  gate [--conf <PATH_TO_CONF_DIR>]\n'
+            + '  gate -h | --help \n'
+            + '  \n'
+            + 'Options: \n'
+            + '  --conf <PATH_TO_CONF_DIR> Load configuration from <PATH_TO_CONF_DIR> (default is "' + DEFAULT_CONF_DIR + '") \n'
+            + '  -h --help                 Shows this help \n';
+
 
 var log = new Logger(
 {
@@ -18,4 +33,11 @@ var log = new Logger(
     ]
 });
 
-progenic.run('gate', 'lib/gate', 1, true, log);
+var cmd = docopt(options);
+
+if (cmd)
+{
+    process.env.CONF_PATH = cmd['--conf'] || DEFAULT_CONF_DIR;
+
+    progenic.run('gate', 'lib/gate', 1, true, log);
+}
